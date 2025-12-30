7c9eb82c7a5bda1a0470e906e2d485ff681694be
Luc Boily <luc.boily@gmail.com>
2025-12-30 07:21:44 -0500

    Fix Next.js 16 headers async redirect

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const headersList = await headers()
-  const lang = headersList.get('accept-language') || ''
+  const lang = headersList.get('accept-language') ?? ''
 
   if (lang.startsWith('fr')) {
     redirect('/fr')
